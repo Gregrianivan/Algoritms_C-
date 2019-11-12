1e36f2857ab8ca6f72002a002964ffdd9ee67637
Gregrianivan <ivan.syrovatskij@gmail.com>
2019-11-12 20:05:13 +0700

    Урок второй Задачи 1,2а,2б

---
+++ Algoritms_C_lesson2_1/Algoritms_C_lesson2_1.cpp
@@ -0,0 +1,32 @@
+﻿/*Реализовать функцию перевода чисел из десятичной системы в двоичную, используя рекурсию.
+Сыроватский Иван*/
+
+#include <iostream>
+
+using namespace std;
+
+void decToBin(int n) {
+	if (n < 0) {
+		cout << "-";
+		decToBin(-n);
+	}
+	else if (n < 2) {
+		cout << n;
+		return;
+	}
+	else {
+		decToBin(n / 2);
+		int res = n % 2;
+		cout << res;
+	}
+}
+
+int main() {
+	int n;
+	cout << "enter number  :\n";
+	cout << "n = ";
+	cin >> n;
+	cout << "binary code:\n";
+	decToBin(n);
+	return 0;
+}
