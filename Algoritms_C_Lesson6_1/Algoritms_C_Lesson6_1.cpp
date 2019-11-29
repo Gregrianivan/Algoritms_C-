f1219c8735402f63858e19c6be099e953d771cfa
Gregrianivan <ivan.syrovatskij@gmail.com>
2019-11-29 22:55:04 +0700

    1.2

---
+++ Algoritms_C_Lesson6_1/Algoritms_C_Lesson6_1.cpp
@@ -0,0 +1,19 @@
+﻿//Реализовать простейшую хэш-функцию. На вход функции подается строка, на выходе получается сумма кодов символов.
+//Сыроватский Иван
+
+#include <iostream>
+#include <string>
+
+std::string Encode(const std::string& s)
+{
+	std::string res(s);
+	for (auto& c : res)
+		c++;
+	return res;
+}
+
+int main()
+{
+	const std::string str = "Ehal Greka cherez reku", str_encoded = Encode(str);
+	std::cout << str << "\nEncode:\n" << str_encoded;
+}
